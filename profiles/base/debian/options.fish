053fe175f6282e33d6e1d5ea1d6e750be899ea85
Senseless Sage <176222722+senseless-sage@users.noreply.github.com>
2025-09-16 19:54:45 +0200

    fix local time, tzdata update issue and add golang and bun to dev profile

--- profiles/base/debian/options.fish
+++ profiles/base/debian/options.fish
@@ -8,7 +8,6 @@ set options \
     -e WAYLAND_DISPLAY \
     -e QT_QPA_PLATFORM=wayland \
     -e XDG_RUNTIME_DIR \
-    -v /etc/localtime:/etc/localtime:ro \
     -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0:ro \
     -v $HOME/.Xauthority:$HOME/.Xauthority:ro \
     -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:ro \
